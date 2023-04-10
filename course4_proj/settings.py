0cfa459659aa22ee29be24874ec3f87e8aef987e
Teerapat Cintornvich <982466+yeahokyok@users.noreply.github.com>
2023-04-10 08:33:45 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -168,3 +168,7 @@ class Dev(Configuration):
 
     CELERY_RESULT_BACKEND = "django-db"
     CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    ADMINS = [("Ben", "ben@example.com")]
--- movies/tasks.py
+++ movies/tasks.py
@@ -1,7 +1,13 @@
 from celery import shared_task
+from django.core.mail import mail_admins
 
 from movies import omdb_integration
 
+
+@shared_task
+def notify_of_new_search_term(search_term):
+    mail_admins("New Search Term", f"A new search term was used: '{search_term}'")
+    
 
 @shared_task
 def search_and_save(search):
